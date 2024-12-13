79240797a77195c77e9b86fd18488c0438235949
BlueCool12 <pmini12@icloud.com>
2024-12-13 17:51:55 +0900

    Ds Store ignore

---
+++ src/main/java/com/pyomin/hood/HoodApplication.java
@@ -0,0 +1,13 @@
+package com.pyomin.hood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pyomin/hood/HoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pyomin.hood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
